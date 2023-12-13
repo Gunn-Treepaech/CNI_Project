461e4c823b90b45cf34d564eb6db0b6dcf70ef6c
Gunn-Treepaech <treepaech@kkumail.com>
2023-12-13 14:50:46 +0700

    add ping

--- CNI.py
+++ CNI.py
@@ -8,9 +8,22 @@ def run_kubectl_command(pod_name, server_pod_ip, package_size):
     except subprocess.CalledProcessError as e:
         print(f"Error running kubectl command: {e}")
 
+def run_ping_command(pod_name, server_pod_ip_ping, package_size_ping):
+    command = f"kubectl exec -it {pod_name} -- ping {server_pod_ip_ping} -l {package_size_ping}"
+    
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running kubectl command: {e}")
+
 # รับข้อมูล pod_name และ server_pod_ip จากผู้ใช้
 pod_name = input("Enter the pod name: ")
 server_pod_ip = input("Enter the server pod IP address: ")
+server_pod_ip_ping = input("Enter the server pod IP Ping address: ")
+
+#----------------------------------------------------------------------------
+# Iperf3
+#----------------------------------------------------------------------------
 
 print("เริ่มทดสอบที่ขนาด 256 Bytes")
 for _ in range(10):
@@ -31,3 +44,27 @@ print("เริ่มทดสอบที่ขนาด 16384 Bytes")
 for _ in range(10):
     run_kubectl_command(pod_name, server_pod_ip, 16384)
 print("เสร็จสิ้นการทดสอบที่ขนาด 16384 Bytes")
+
+#----------------------------------------------------------------------------
+# Ping
+#----------------------------------------------------------------------------
+
+print("เริ่มทดสอบ Ping ที่ขนาด 256 Bytes")
+for _ in range(10):
+    run_ping_command(pod_name, server_pod_ip, 256)
+print("เสร็จสิ้นการทดสอบ Ping ที่ขนาด 256 Bytes")
+
+print("เริ่มทดสอบ Ping ที่ขนาด 1024 Bytes")
+for _ in range(10):
+    run_ping_command(pod_name, server_pod_ip, 1024)
+print("เสร็จสิ้นการทดสอบ Ping ที่ขนาด 1024 Bytes")
+
+print("เริ่มทดสอบ Ping ที่ขนาด 4096 Bytes")
+for _ in range(10):
+   run_ping_command(pod_name, server_pod_ip, 4096)
+print("เสร็จสิ้นการทดสอบ Ping ที่ขนาด 4096 Bytes")
+
+print("เริ่มทดสอบ Ping ที่ขนาด 16384 Bytes")
+for _ in range(10):
+    run_ping_command(pod_name, server_pod_ip, 16384)
+print("เสร็จสิ้นการทดสอบ Ping ที่ขนาด 16384 Bytes")
--- README.md
+++ README.md
@@ -5,19 +5,19 @@
 ## ipref3 with Kubernetes
 * https://www.suse.com/support/kb/doc/?id=000020954
   ## Project Commad:
-     ### แก้ hostname บน Linux:
+     ### แก้โฮสต์ของคอมพิวเตอร์ Linux:
           sudo nano /etc/hostname	
      ### ติ้ดตั้ง microk8s:
           sudo snap install microk8s --classic
      ### ถอนติ้ดตั้ง microk8s:
           sudo snap remove microk8s
-     ### ดูสถานะของ microk8s:
+     ### แสดงสถานะของ MicroK8s:
           microk8s status
      ### สร้างนามแฝง (alias) ให้กับคำสั่ง microk8s.kubectl ให้เป็น kubectl แทน:
           sudo snap alias microk8s.kubectl kubectl
      ### ดูสถานะ node ทั้งหมดใน cluster:
           kubectl get nodes
-     ### Get ข้อมูลสำหรับเพิ่มเข้า cluster ใน node ที่เป็น master:
+     ### ใช้เพิ่มโหนด (node) ใหม่ให้กับ MicroK8s:
           sudo microk8s.add-node
      ### ลบ node ออกจาก cluster ใน master:
           sudo microk8s remove-node <node name> 
