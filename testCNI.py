450a0781c216f0c75522945cfec6825a130e878c
Gunn-Treepaech <treepaech@kkumail.com>
2024-02-28 15:38:28 +0700

    add test new

--- testCNI.py
+++ testCNI.py
@@ -1,20 +1,21 @@
 import subprocess
+import asyncio
 
-def run_command(command):
+async def run_command(command):
     try:
         subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running command: {e}")
 
-def run_kubectl_command(pod_name, server_pod_ip, package_size):
+async def run_kubectl_command(pod_name, server_pod_ip, package_size):
     command = f"kubectl exec -it {pod_name} -- iperf3 -c {server_pod_ip} -p 12345 -f k -n {package_size} "
     run_command(command)
 
-def run_ping_command(pod_name, server_pod_ip, package_size_ping):
+async def run_ping_command(pod_name, server_pod_ip, package_size_ping):
     command = f"kubectl exec -it {pod_name} -- ping -c 1 {server_pod_ip} -s {package_size_ping}"
     run_command(command)
 
-def run_tests(pod_name, server_pod_ip, package_sizes):
+async def run_tests(pod_name, server_pod_ip, package_sizes):
     for size in package_sizes:
         counter = 1
         print(f"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
@@ -42,6 +43,9 @@ if __name__ == "__main__":
     # Get user input
     pod_name = input("ใส่ชื่อ pod ที่จะเข้าไปใช้คำสั่ง: ")
     server_pod_ip = input("ใส่ IP ของ Server Iperf: ")
+    
+    all_pod_name = pod_name.split()
+    len_all_pod_name = len(all_pod_name)
 
     # ------ Define package sizes for both iperf3 and ping ------
     # ขนาดเล็ก (100 Byte): ขนาดแพ็กเก็ตขนาดเล็กเหมาะสำหรับการทดสอบความเร็วในการส่งข้อมูลแบบ bursty หรือการส่งข้อมูลจำนวนมากในเวลาสั้นๆ
@@ -56,4 +60,8 @@ if __name__ == "__main__":
     ping_package_sizes = ["100_ping", "1000_ping", "10000_ping"]
 
     # Run tests
-    run_tests(pod_name, server_pod_ip, iperf3_package_sizes + ping_package_sizes)
+    if len_all_pod_name >= 1:
+        for i in all_pod_name:
+            run_tests(i, server_pod_ip, iperf3_package_sizes + ping_package_sizes)
+    else:
+        run_tests(pod_name, server_pod_ip, iperf3_package_sizes + ping_package_sizes)
