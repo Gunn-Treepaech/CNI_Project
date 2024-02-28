a0465d80acb12e49c3155aef89469fa5866dc290
Gunn-Treepaech <treepaech@kkumail.com>
2024-02-28 19:35:28 +0700

    add new

--- testCNI.py
+++ testCNI.py
@@ -1,18 +1,19 @@
 import subprocess
+from threading import Thread
 
-def run_command(command):
+def run_command(command, pod_name, size, counter):
     try:
         subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Error running command: {e}")
+        print(f"Error running command for {size} round {counter} in pod {pod_name}: {e}")
 
-def run_kubectl_command(pod_name, server_pod_ip, package_size):
+def run_kubectl_command(pod_name, server_pod_ip, package_size, counter):
     command = f"kubectl exec -it {pod_name} -- iperf3 -c {server_pod_ip} -p 12345 -f k -n {package_size} "
-    run_command(command)
+    return Thread(target=run_command, args=(command, pod_name, package_size, counter))
 
-def run_ping_command(pod_name, server_pod_ip, package_size_ping):
+def run_ping_command(pod_name, server_pod_ip, package_size_ping, counter):
     command = f"kubectl exec -it {pod_name} -- ping -c 1 {server_pod_ip} -s {package_size_ping}"
-    run_command(command)
+    return Thread(target=run_command, args=(command, pod_name, package_size_ping, counter))
 
 def run_tests(pod_name, server_pod_ip, package_sizes):
     for size in package_sizes:
@@ -26,35 +27,31 @@ def run_tests(pod_name, server_pod_ip, package_sizes):
                 print(f"{size} Ping Test round {counter}")
                 print(f"----------------------------------------------------------------------")
                 # Code
-                run_ping_command(pod_name, server_pod_ip, size.split('_')[0])
+                thread = run_ping_command(pod_name, server_pod_ip, size.split('_')[0], counter)
             else:
                 print(f"----------------------------------------------------------------------")
                 print(f"{size} Test round {counter}")
                 print(f"----------------------------------------------------------------------")
                 # Code
-                run_kubectl_command(pod_name, server_pod_ip, size)
+                thread = run_kubectl_command(pod_name, server_pod_ip, size, counter)
+
+            thread.start()
+            thread.join()  # Wait for the completion of the current command before starting the next one
             counter += 1
+
         print(f"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
         print(f"Test completed with package size {size} Bytes")
         print(f"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
 if __name__ == "__main__":
     # Get user input
-    pod_name = input("ใส่ชื่อ pod ที่จะเข้าไปใช้คำสั่ง: ")
-    server_pod_ip = input("ใส่ IP ของ Server Iperf: ")
+    pod_name = input("Enter the pod name to run commands: ")
+    server_pod_ip = input("Enter the IP of the Iperf server: ")
     
     all_pod_name = pod_name.split()
     len_all_pod_name = len(all_pod_name)
 
     # ------ Define package sizes for both iperf3 and ping ------
-    # ขนาดเล็ก (100 Byte): ขนาดแพ็กเก็ตขนาดเล็กเหมาะสำหรับการทดสอบความเร็วในการส่งข้อมูลแบบ bursty หรือการส่งข้อมูลจำนวนมากในเวลาสั้นๆ
-    # ขนาดกลาง (1,000 Byte): ขนาดแพ็กเก็ตขนาดกลางเป็นขนาดแพ็กเก็ตมาตรฐานที่มักใช้ในการทดสอบเครือข่าย
-    # ขนาดใหญ่ (10,000 Byte): ขนาดแพ็กเก็ตขนาดใหญ่เหมาะสำหรับการทดสอบ throughput ของเครือข่าย
-    # ขนาดพิเศษ (100000 Byte): ขนาดแพ็กเก็ตพิเศษเหมาะสำหรับการทดสอบ throughput ของเครือข่ายที่มีแบนด์วิดท์สูง
-    # ------ END ------
-
-    #iperf3_package_sizes = ["100M"]
-    #ping_package_sizes = []
     iperf3_package_sizes = ["100", "1000", "10000", "100000"]
     ping_package_sizes = ["100_ping", "1000_ping", "10000_ping"]
 
